e1c6217b4d2e9b373190898997248093e882987f
cmirnov <cmirnov239kirill@gmail.com>
2015-12-17 20:54:13 +0300

    Update hashtable.c

--- hashtable.c
+++ hashtable.c
@@ -1,293 +1,230 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<time.h>
+#include<math.h>
+#include<string.h>
 
-#define MAXN 100000
+struct list{                           
+    int val;
+    long long hash;
+    struct list *next;
+}; typedef struct list list;
 
 
+struct hash_array{                       
+    list *head;
+}; typedef struct hash_array array;
 
-	
-typedef struct node {
-	int val;
-	struct node *next;
-} node;
+struct hash_table{                       
+    int size;                            
+    array *arr;                          
+    int (*callback)(char*);              
+}; typedef struct hash_table table;
+table tbl;
 
-void set(node **a, int hash, int data){
+typedef int (*callback_param_func)(char *param); 
 
-	node *t = (node*)malloc(sizeof(node));
-	
-	if (t == NULL){
-		print("run out of memory\n");
-		return;
-	}
-	
-	t->val = data;
-	
-	node *head = a[hash];
-	
-	if (t == 0){
-		printf("run out of memory\n");
-		return;
-	}
-	
-	if (a[hash] == NULL){
-		t->next = NULL;
-		a[hash] = t;
-		return; 	
-	}
-	
-	else{
-		node *last;
-		last = a[hash];
-		if (last->next == NULL){
-			last->next = t;
-			t->next = NULL;
-			return;
-		}
-		else{
-			while (last->next->next != NULL){
-				last = last->next;
-			}
-			last->next->next = t;
-			t->next = NULL;
-		}
-	}
-	return;
+int call_hash_func(callback_param_func func, char *param){
+    return func(param);
+}
+
+void clean(list *cur){
+	list *tmp;                  
+    while (cur != NULL){
+        tmp = cur;
+        cur = cur->next;
+        free(tmp);
+    }
+    return;
+}
+
+list *find(char *c){                          
+    long long Hsh = call_hash_func(tbl.callback, c);
+    int pos = (Hsh % tbl.size + tbl.size) % tbl.size;
+    list *cur = tbl.arr[pos].head;
+    list *tmp;
+    while (cur->next != NULL){
+        tmp = cur->next;
+		if (tmp->hash == Hsh){
+            break;
+        }
+        cur = cur->next;
+    }
+    return cur;
 }
 
 
-void get(FILE* w, node **a, int hash){	
-	if (a[hash] == NULL){
-		fprintf(w, "Empty\n");
-		return;
-	}
-	node *cur;
-	cur = a[hash];
-	
-	while (cur->next != NULL){
-		fprintf(w, "%d\t", cur->val);
-		cur = cur->next;
-	}
-	fprintf(w, "%d\n", cur->val);
-	return;
+int max(int a, int b){ 
+   if (a > b){
+        return a;
+    } else {
+        return b;
+    }
 }
 
-
-void rem (node **head, int hash, int size){
-	node *t = head[hash];
-	if	(head[hash] == NULL){
-		printf("Empty\n");
-		return;
-	}
-	if (t->val == size){
-		head[hash] = t->next;
-		free(t);
-		return;		
-	}
-	while (t->next->val != size && t->next->next != NULL){
-		t = t->next;
-	}
-	if (t->next->val == size){
-		node *cur = t->next;
-		t->next = t->next->next;
-		free(cur);
-		return;
-	}
-	if (t->next->next == NULL){
-		printf("not found\n");
-		return;
-	}
-	return;
-}
-
-void clear (node **head){
-	int i;
-	for (i = 0; i < MAXN; ++i){
-		node *t = head[i];
-		while(head[i] != NULL){
-			head[i] = (head[i])->next;
-			free(t);
-			t = head[i];
-		}
-	}
-	return;
-}
-
-
-void print(FILE *w, node **a){
-	int i;
-	for (i = 0; i < MAXN; ++i){
-		get(w, a, i);
-	}
-}
-
-
-int len(node **head, int hash){
-	int res = 1;
-	node *t = head[hash];
-	if (t == NULL){
-		return 0;
-	}
-	while (t->next != NULL){
-		t = t->next;
-		res++;
-	}
-	return res;
-}
-
-void printans(FILE *w, node **a){
-	int i, t;
-	for (i = 0; i < MAXN; ++i){
-		t = len(a, i);
-		if (t == 0){
-			//fprintf(w, "Empty\n");
-		}
-		else{
-			fprintf(w, "%d\n", t);
-		}
-	}
-	return;
-}
-
-void statistics (FILE *w, node **head){
-	int i;
-	int num, nonempt = 0, min = MAXN, max = 0, t;
-	for (i = 0; i < MAXN; ++i){
-		t = len(head, i);
-		num += t;
-		if (t != 0){
-			nonempt++;
-		}
-		if (min > t && t > 0){
-			min = t;
-		}
-		if (max < t){	
-			max = t;
-		}
-	}
-	fprintf(w, "non empty = %d\n", nonempt);
-	fprintf(w, "num = %d\n", num);
-	fprintf(w, "mid = %f\n", ((float)num/(float)MAXN));
-	fprintf(w, "mid without empty = %f\n", (float)num/(float)nonempt);
-	fprintf(w, "min = %d\n", min);
-	fprintf(w, "max = %d\n", max);
-	return;
-}	
-
-////////////////////////////////////////////////////////////////////////////////////
-
-int hash1 (char *s, int size){
-	return 1;
-}
-
-int hash2 (char *s, int size){
-	int res = 0;
-	int i;
-	for (i = 0; i < size; ++i){
-		res += s[i];
-	}
-	return res;
-}
-
-int hash3 (char *s, int size){
-	int div = 1 << 22, q = 1 << 8;
-	int res = 0;
-	int i;
-	for (i = 0; i < size; ++i){
-		res *= q;
-		res += s[i];
-	}
-	return res;
+int min(int a, int b){  
+    if (a < b){
+        return a;
+    } else {
+        return b;
+    }
 }
 
 
 
 
-////////////////////////////////////////////
-
-
-
-
-int main() {
-	
-	FILE *r = fopen("input.txt", "r");
-	FILE *w = fopen("output1.txt", "w");
-	
-	if (r == NULL){
-		printf("u looser\n");
-		return 0;
-	}
-	node *a[MAXN];
-	int i;
-	for (i = 0; i < MAXN; ++i){
-		a[i] = NULL;
-	}
-	
-	int start_time, end_time;
-	start_time = clock();
-	char s[256];
-	int hash;
-	int c;
-	int j  = 0, size;
-	while (c != EOF && (c = fgetc(r))){
-		if (isalpha(c)){
-			s[j] = c;
-			j++;
-		}
-		else{
-			s[j] = '\0'; 
-			size = j;
-			j = 0;
-		}
-		if (j == 0 && size != 0){
-			hash = hash1(s, strlen(s));
-			hash %= MAXN;
-			set(a, hash, size);
-		}
-	}
-	
-	end_time = clock();
-	
-	//statistics (w, a);
-	//clear (a);
-	
-	printans(w, a);
-
-	
-	fprintf(w, "%d", end_time - start_time);
-	return 0;
+void create(int(*func)(char*), int num){            
+    int i;
+    tbl.size = num;
+    tbl.arr = (array*)malloc(num * sizeof(array));
+    tbl.callback = func;
+    for (i = 0; i < num; ++i){
+        tbl.arr[i].head = malloc(sizeof(list));
+        (tbl.arr[i].head)->next = NULL;
+        (tbl.arr[i].head)->val = 0;
+    }
+    return;
 }
 
+void del(){                                           
+    int i;
+    int size = tbl.size;
+    for (i = 0; i < size; ++i){
+        clean(tbl.arr[i].head);
+    }
+    free(tbl.arr);
+    return;
+}
 
+void add(char *c){                                      
+    list *cur = find(c);
+    if (cur->next == NULL){ 
+        list *nw = (list*)malloc(sizeof(list));
+        cur->next = nw;
+        nw->next = NULL;
+        nw->val = 1;
+        nw->hash = call_hash_func(tbl.callback, c);;
+    } else {
+        cur = cur->next;
+        cur->val++;
+    }
+    return;
+}
 
-///////////////////
-/*
-hash1
-non empty = 1
-num = 32771
-mid = 0.327710
-mid without empty = 32771.000000
-min = 32770
-max = 32770
-time = 2281
+void erase(char *c){  
+    list *cur = find(c);
+    if (cur->next == NULL){
+        printf("fail\n");
+    } else {
+        cur = cur->next;
+        if (cur->val == 0){
+            printf("fail\n");
+        } else {
+            cur->val--;
+        }
+    }
+    return;
+}
 
+void val(char *c){
+    list *cur = find(c);
+    printf("number");
+    if (cur->next == NULL){
+        printf("0\n");
+    } else {
+        cur = cur->next;
+        printf("%d\n", cur->val);
+    }
+    return;
+}
 
-hash2
-non empty = 888
-num = 32771
-mid = 0.327710
-mid without empty = 36.904278
-min = 1
-max = 999
-time = 79
+void statistic(){
+    int i = 0;
+    int number_cells = 0;
+    int number_elem = 0;
+    int sum_len_chain = 0;
+    int max_len_chain = 0;
+    int min_len_chain = 1e9;
+    int max_elem = 0;
+    int mid_len_chain = 0;
+    for (i = 0; i < tbl.size; ++i){
+        list *cur = tbl.arr[i].head;
+        int cur_len = 0;
+        while (cur != NULL){
+            cur_len++;
+            number_elem += cur->val;
+            max_elem = max(max_elem, cur->val);
+            cur = cur->next;
+        }
+        cur_len--;
+        if (cur_len > 0){
+            number_cells++;
+            min_len_chain = min(min_len_chain, cur_len);
+        }
+        sum_len_chain += cur_len;
+        max_len_chain = max(max_len_chain, cur_len);
+    }
+    if (number_cells != 0){
+        mid_len_chain = sum_len_chain / number_cells;
+    }
+    if (min_len_chain == 1e9){
+        min_len_chain = -1;
+    }
+    printf("Number of using cells: %d\n", number_cells);
+    printf("Number of elements: %d\n", number_elem);
+    printf("Average length of chains: %d\n", mid_len_chain);
+    printf("Minimal length of chains: %d\n", min_len_chain);
+    printf("Maximal length of chains: %d\n", max_len_chain);
+    printf("Maximal element: %d\n", max_elem);
+    return;
+}
 
-hash3
-non empty = 2656
-num = 32771
-mid = 0.327710
-mid without empty = 12.338479
-min = 1
-max = 992
-time = 60
-*/
+/////////////////////////////////////////////////////////
+
+int hash1(char *c){           
+    return 3;
+}
+
+int hash2(char *c){            
+    int sum = 0;
+    while (*c){
+        sum += *c;
+        c++;
+    }
+    return sum;
+}
+
+int hash3(char *c){              
+    const int p = 300;            
+    long long hash = 0;
+    int i;
+    for (i = 0; i < strlen(c); ++i){
+    	hash *= p;
+    	hash += c[i];
+	}
+    return hash;
+}
+
+/////////////////////////////////////////////////////////
+
+int main()
+{
+    double start = clock();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    const int size = 42*42*42;
+    create(hash3, size);
+    char c[100];
+    while (scanf("%s", c) != EOF){
+        size_t len = strlen(c) - 1;
+        while (len >= 0 && !isalpha(c[len])){ 
+            c[len] = '\0';
+            len--;
+        }
+        add(c);
+    }
+    statistic();
+    del();
+    printf("%f", (clock() - start) / CLOCKS_PER_SEC);
+    return 0;
+}
